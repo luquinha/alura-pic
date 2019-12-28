063234e04f1f9b1cc7f3008f73d356ecc39e186a
Lucas M Fonseca <lucas.fonseca@dextra-sw.com>
2019-12-28 17:26:39 -0300

    First code commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="photo of photos">
+        <img v-bind:src="photo.url" v-bind:alt="photo.titulo">
+      </li>
+    </ul>    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Alurapic',
+      photos: [
+        {
+          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQJx-b8pkUH5XODzW0YoDoAPGUn5L8z7bgO7xaCDCbZNu74F6qK',
+          title: 'Happy dog'
+        },
+        {
+          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQJx-b8pkUH5XODzW0YoDoAPGUn5L8z7bgO7xaCDCbZNu74F6qK',
+          title: 'Happy dog'
+        }
+      ]
+    }
+  },
+  created() {
+    this.$http.get
+  }
+}
+</script>
+
+<style>
+</style>
